e4698557b42bf46eb10fe0ffe7669cdca70c0191
Mrcosgun <mrcosgun9@gmail.com>
2019-01-04 22:44:11 +0300

    açılır menü eklendi

--- SakalliTicaret.UI.WEB/Controllers/CompanentController.cs
+++ SakalliTicaret.UI.WEB/Controllers/CompanentController.cs
@@ -15,7 +15,7 @@ namespace SakalliTicaret.UI.WEB.Controllers
         // GET: Companent
         public ActionResult SliderMenu()
         {
-            var Category = db.Categories.Where(x => x.ParentId == null).OrderBy(x => x.Name).ToList();
+            var Category = db.Categories.OrderBy(x => x.Name).ToList();
             return View(Category);
         }
 
