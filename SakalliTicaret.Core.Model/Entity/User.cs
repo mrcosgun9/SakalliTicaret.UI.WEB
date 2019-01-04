aa7d2129187b213eb02cd36d52fd3c25ad07f819
Mrcosgun <mrcosgun9@gmail.com>
2019-01-04 15:19:33 +0300

    validate eklendi

--- SakalliTicaret.Core.Model/Entity/User.cs
+++ SakalliTicaret.Core.Model/Entity/User.cs
@@ -10,16 +10,25 @@ namespace SakalliTicaret.Core.Model.Entity
 {
     public class User : EntityBase
     {
+        [Required]
         [Display(Name = "İsim")]
         public string Name { get; set; }
+        [Required]
         [Display(Name = "Soyisim")]
         public string LastName { get; set; }
+        [Required]
         [Display(Name = "Email")]
+        [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}" +
+                           @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" +
+                           @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$",
+            ErrorMessage = "Email adresi geçersiz")]
         public string Email { get; set; }
         [Display(Name = "Resim")]
         public string ImageUrl { get; set; }
         [Display(Name = "Telefon")]
         public string Telephone { get; set; }
+        [Required]
+        [StringLength(5)]
         [Display(Name = "Şifre")]
         public string Password { get; set; }
         [Display(Name = "Tc Numarası")]
