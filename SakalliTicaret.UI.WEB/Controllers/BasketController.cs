e6d29411536a652db24e1e38ccf3571739bc6182
mrcosgun <mrcosgun9@gmail.com>
2019-04-07 23:33:06 +0300

    lkj

--- SakalliTicaret.UI.WEB/Controllers/BasketController.cs
+++ SakalliTicaret.UI.WEB/Controllers/BasketController.cs
@@ -223,8 +223,7 @@ namespace SakalliTicaret.UI.WEB.Controllers
             // !!! Eğer bu örnek kodu sunucuda değil local makinanızda çalıştırıyorsanız
             // buraya dış ip adresinizi (https://www.whatismyip.com/) yazmalısınız. Aksi halde geçersiz paytr_token hatası alırsınız.
             //string user_ip = GetIPAddress();
-
-            string user_ip = "78.186.172.90";
+            string user_ip = "88.230.136.125";
             if (user_ip == "" || user_ip == null)
             {
                 user_ip = Request.ServerVariables["REMOTE_ADDR"];
